db2a9c08490edf04370d0714359ffa9610495fa6
Julio Enrique Rojas Ramos <98976732+yker11@users.noreply.github.com>
2022-02-09 17:49:46 -0500

    Initial commit

---
+++ src/main/java/com/example/ActiveProducts/ActiveProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ActiveProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ActiveProducts/ActiveProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ActiveProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
